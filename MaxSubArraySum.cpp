273f2deb6df4814f4c2d24656a02db40a8d77954
satyajeet <satyajeetrajupali@gmail.com>
2021-05-05 18:28:49 +0530

    Few Changes

--- MaxSubArraySum.cpp
+++ MaxSubArraySum.cpp
@@ -1,29 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxSubarraySum(int arr[], int n){
-        
-        int max = INT_MIN;
-        int sum=0;
-        // Your code here
-        for(int i=0; i<n; i++){
-            sum = sum + arr[i];
-            if(max<sum){
-                max = sum;
-            }
-            if(sum<0){
-                sum = 0;
-            }
+
+// Kadanes Algorithm
+int maxSubarraySum(int arr[], int n)
+{
+
+    int max = INT_MIN;
+    int sum = 0;
+    // Your code here
+    for (int i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+        if (max < sum){
+            max = sum;
         }
-        
-        return sum;
+        if (sum < 0){
+            sum = 0;
+        }
     }
+
+    return max;
+}
 
 int main(){
 
-    int arr[] = {1,2,3,-2,5};
-    int n = sizeof(arr)/sizeof(arr[0]);
-
-    cout<<"Max Sum is: "<<maxSubarraySum(arr,n);
+    int arr[] = {1, 2, 3, -2, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Max Sum is: " << maxSubarraySum(arr, n);
     return 0;
+    
 }
