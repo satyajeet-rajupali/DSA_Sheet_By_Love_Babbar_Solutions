d87a209cbba8eb7416cb3db0bcc80fda64baa518
satyajeet <satyajeetrajupali@gmail.com>
2022-04-17 12:08:22 +0530

    M

--- Tree/Question34.cpp
+++ Tree/Question34.cpp
@@ -73,8 +73,8 @@ bool isIsomorphic(Node *root1, Node *root2)
     if (!root1 or !root2)
         return false;
 
-    if (root1->data == root2->data)
-        return true;
+    if (root1->data != root2->data)
+        return false;
 
     bool isomorphism = (isIsomorphic(root1->left, root2->right) and isIsomorphic(root1->right, root2->left));
     bool mirrorism = (isIsomorphic(root1->left, root2->left) and isIsomorphic(root1->right, root2->right));
