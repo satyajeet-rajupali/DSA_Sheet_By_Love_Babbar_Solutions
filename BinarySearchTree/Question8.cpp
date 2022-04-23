c2ef77d56ef7d8c0d84562ffe10307b05b79deb8
satyajeet <satyajeetrajupali@gmail.com>
2022-04-23 11:17:30 +0530

    8, 9

--- BinarySearchTree/Question8.cpp
+++ BinarySearchTree/Question8.cpp
@@ -6,251 +6,154 @@ class Node
 public:
     Node *left;
     int data;
-    int height;
     Node *right;
 };
 
-class AVLTree
+class BST
 {
 public:
     Node *root;
-    AVLTree() { root = nullptr; };
-    int NodeHeight(Node *p);
-    int BalanceFactor(Node *p);
-    Node *inorderSuccessor(Node *p);
-    Node *inorderPredecessor(Node *p);
+    BST() { root = nullptr; }
 
-    Node *LLRotation(Node *p);
-    Node *RRRotation(Node *p);
-    Node *LRRotation(Node *p);
-    Node *RLRotation(Node *p);
+    // To insert elements in the Binary Search Tree (BST).
+    void insert(int key);               // Iterative
+    Node *rInsert(Node *root, int key); // Recursive
 
-    void inorder(Node *p);
-    Node *insert(Node *p, int key);
-    Node *Delete(Node *p, int key);
+    Node *newNode(int key);
+
+    // To display the elements of Binary Search Tree
+    void inorder(Node *root);
+
+    Node *binaryTreeToBST(Node *root);
+    void inorderBinaryTree(Node *root, vector<int> &nodes);
+    void inorderBinarySearchTree(Node *root, vector<int> &nodes, int &i);
 };
 
-int AVLTree::NodeHeight(Node *p)
+Node *BST::newNode(int key)
 {
-    int hl;
-    int hr;
-
-    hl = (p and p->left) ? p->left->height : 0;
-    hr = (p and p->right) ? p->right->height : 0;
-
-    return (hl > hr ? hl + 1 : hr + 1);
+    Node *t = new Node;
+    t->data = key;
+    t->left = nullptr;
+    t->right = nullptr;
+    return t;
 }
 
-int AVLTree::BalanceFactor(Node *p)
+void BST::insert(int key)
 {
-    int hl;
-    int hr;
-
-    hl = (p and p->left) ? p->left->height : 0;
-    hr = (p and p->right) ? p->right->height : 0;
-
-    return hl - hr;
-}
-
-Node *AVLTree::inorderSuccessor(Node *p)
-{
-    while (p and p->left)
-        p = p->left;
-    return p;
-}
-
-Node *AVLTree::inorderPredecessor(Node *p)
-{
-    while (p and p->right)
-        p = p->right;
-    return p;
-}
-
-Node *AVLTree::LLRotation(Node *p)
-{
-    Node *pl = p->left;
-    Node *plr = pl->right;
-
-    pl->right = p;
-    p->left = plr;
-
-    p->height = NodeHeight(p);
-    pl->height = NodeHeight(pl);
-
-    if (root == p)
-        root = pl;
-    return pl;
-}
-
-Node *AVLTree::RRRotation(Node *p)
-{
-    Node *pr = p->right;
-    Node *prl = pr->left;
-
-    pr->left = p;
-    p->right = prl;
-
-    p->height = NodeHeight(p);
-    pr->height = NodeHeight(pr);
-
-    if (root == p)
-        root = pr;
-    return pr;
-}
-
-Node *AVLTree::LRRotation(Node *p)
-{
-    Node *pl = p->left;
-    Node *plr = pl->right;
-
-    pl->right = plr->left;
-    p->left = plr->right;
-
-    plr->left = pl;
-    plr->right = p;
-
-    pl->height = NodeHeight(pl);
-    p->height = NodeHeight(p);
-    plr->height = NodeHeight(plr);
-
-    if (root == p)
-        root = plr;
-    return plr;
-}
-
-Node *AVLTree::RLRotation(Node *p)
-{
-    Node *pr = p->right;
-    Node *prl = pr->left;
-
-    p->right = prl->left;
-    pr->left = prl->right;
-
-    prl->left = p;
-    prl->right = pr;
-
-    p->height = NodeHeight(p);
-    pr->height = NodeHeight(pr);
-    prl->height = NodeHeight(prl);
-
-    if (root == p)
-        root = prl;
-
-    return prl;
-}
-
-void AVLTree::inorder(Node *p)
-{
-    if (!p)
-        return;
-    inorder(p->left);
-    cout << p->data << " ";
-    inorder(p->right);
-}
-
-Node *AVLTree::insert(Node *p, int key)
-{
-
-    Node *t;
-
-    if (!p)
+    Node *p, *q, *t;
+    if (!root)
     {
         t = new Node;
         t->data = key;
-        t->height = 1;
+        t->left = nullptr;
+        t->right = nullptr;
+        root = t;
+        return;
+    }
+    p = root;
+    while (p)
+    {
+        q = p;
+
+        if (key < p->data)
+            p = p->left;
+        else if (key > p->data)
+            p = p->right;
+        else
+            return;
+    }
+
+    t = new Node;
+    t->data = key;
+    t->left = t->right = nullptr;
+
+    if (key < q->data)
+        q->left = t;
+    else if (key > q->data)
+        q->right = t;
+
+    return;
+}
+
+Node *BST::rInsert(Node *root, int key)
+{
+    Node *t;
+    if (!root)
+    {
+        t = new Node;
+        t->data = key;
         t->left = nullptr;
         t->right = nullptr;
         return t;
     }
 
-    if (key < p->data)
-        p->left = insert(p->left, key);
-    else if (key > p->data)
-        p->right = insert(p->right, key);
+    if (key < root->data)
+        root->left = rInsert(root->left, key);
+    else if (key > root->data)
+        root->right = rInsert(root->right, key);
 
-    p->height = NodeHeight(p);
-
-    if (BalanceFactor(p) == 2 and BalanceFactor(p->left) == 1)
-        return LLRotation(p);
-    else if (BalanceFactor(p) == 2 and BalanceFactor(p->left) == -1)
-        return LRRotation(p);
-    else if (BalanceFactor(p) == -2 and BalanceFactor(p->right) == -1)
-        return RRRotation(p);
-    else if (BalanceFactor(p) == -2 and BalanceFactor(p->right) == 1)
-        return RLRotation(p);
-
-    return p;
+    return root;
 }
 
-Node *AVLTree::Delete(Node *p, int key)
+void BST::inorder(Node *root)
 {
-    if (!p)
-        return p;
+    if (!root)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
 
-    if (!p->left and !p->right)
-    {
-        if (root == p)
-            root = nullptr;
-        delete p;
-        return nullptr;
-    }
+void BST::inorderBinaryTree(Node *root, vector<int> &nodes)
+{
+    if (!root)
+        return;
+    inorderBinaryTree(root->left, nodes);
+    nodes.push_back(root->data);
+    inorderBinaryTree(root->right, nodes);
+}
 
-    if (key < p->data)
-        p->left = Delete(p->left, key);
-    else if (key > p->data)
-        p->right = Delete(p->right, key);
-    else
-    {
-        Node *q;
-        if (NodeHeight(p->left) > NodeHeight(p->right))
-        {
-            q = inorderPredecessor(p->left);
-            p->data = q->data;
-            p->left = Delete(p->left, q->data);
-        }
-        else
-        {
-            q = inorderSuccessor(p->right);
-            p->data = q->data;
-            p->right = Delete(p->right, q->data);
-        }
-    }
+void BST::inorderBinarySearchTree(Node *root, vector<int> &nodes, int &i)
+{
+    if (!root)
+        return;
+    inorderBinarySearchTree(root->left, nodes, i);
+    root->data = nodes[i++];
+    inorderBinarySearchTree(root->right, nodes, i);
+}
 
-    p->height = NodeHeight(p);
+Node *BST::binaryTreeToBST(Node *root)
+{
 
-    if (BalanceFactor(p) == 2 and BalanceFactor(p->left) == 0) // L0
-        return LLRotation(p);
-    else if (BalanceFactor(p) == 2 and BalanceFactor(p->left) == 1) // L1
-        return LLRotation(p);
-    else if (BalanceFactor(p) == 2 and BalanceFactor(p->left) == -1)
-        return LRRotation(p);
-    else if (BalanceFactor(p) == -2 and BalanceFactor(p->right) == 0)
-        return RRRotation(p);
-    else if (BalanceFactor(p) == -2 and BalanceFactor(p->right) == -1)
-        return RRRotation(p);
-    else if (BalanceFactor(p) == -2 and BalanceFactor(p->right) == 1)
-        return RLRotation(p);
-    return p;
+    if (!root)
+        return root;
+
+    vector<int> nodes;
+    int i = 0;
+
+    inorderBinaryTree(root, nodes);
+    sort(nodes.begin(), nodes.end());
+    inorderBinarySearchTree(root, nodes, i);
+
+    return root;
 }
 
 int main()
 {
+    BST bst;
+    bst.root = bst.newNode(1);
+    bst.root->left = bst.newNode(2);
+    bst.root->right = bst.newNode(3);
+    (bst.root->left)->left = bst.newNode(4);
 
-    int arr[] = {10, 20, 30, 25, 28, 27, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    AVLTree avt;
-    for (int i = 0; i < n; i++)
-        avt.root = avt.insert(avt.root, arr[i]);
-
-    cout << "Elements in AVL Tree:\n";
-    avt.inorder(avt.root);
+    cout << "Elements in BST:\n";
+    bst.inorder(bst.root);
     cout << "\n";
 
-    avt.Delete(avt.root, 28);
+    Node *root = bst.binaryTreeToBST(bst.root);
 
-    cout << "Elements in AVL Tree Post Deletion:\n";
-    avt.inorder(avt.root);
+    cout << "Elements in BST post conversion:\n";
+    bst.inorder(bst.root);
     cout << "\n";
 
     return 0;
